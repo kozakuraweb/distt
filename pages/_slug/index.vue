5986b8f4eab18b0b10364276be30b3b51b071518
tsuchida ayako <info@koza-kura.com>
2021-09-09 21:03:55 +0900

    recommit

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -8,15 +8,16 @@
 
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
+
 export default {
-  async asyncData({ $config,params }) {
+  async asyncData({ params }) {
     const { data } = await axios.get(
       `https://koza-kura.microcms.io/api/v1/blog/${params.slug}`,
       {
-        headers: { 'X-API-KEY': $config.apiKey }
+        headers: { 'X-API-KEY': 'f62096f1-3b69-446f-8e5d-6f02f0a20f09' }
       }
-    );
+    )
     return data
   }
 }
